42153ae028232ee84a259b63606be707c94dea3a
Fajarullah <frullah12@gmail.com>
2021-07-20 23:57:57 +0700

    add layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount } from "svelte";
+
+  import "virtual:windi.css";
+
+  if (!import.meta.env.PROD) {
+    onMount(() => {
+      // @ts-ignore
+      import("virtual:windi-devtools");
+    });
+  }
+</script>
+
+<main class="p-4">
+  <slot />
+</main>
